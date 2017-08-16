6ac1c0ba54f18c98b77c71a9d6eb0f7f4729e176
genewitch <genewitch@gmail.com>
2017-08-16 17:52:49 -0500

    Update awk2dictfiles.sh

--- awk2dictfiles.sh
+++ awk2dictfiles.sh
@@ -5,6 +5,10 @@
 #      cat ANSI-BASIC-syntax-manual.txt | grep SYNTAX  | awk '{ print $2 }' > awked.txt
 #           ^ found this online           ^header line            ^ first column after "SYNTAX"
 #
+# Next steps:
+#      $ cp awked.txt ~/afl/testprogram/in && cd ~/afl/testprogram/in
+#      $ ./awk2dictfiles.sh
+#      $ rm awked.txt # and possibly && rm awk2dictfiles.sh, whatever. You get it.
 
 for fn in `cat awked.txt`; do
     echo "the next file is $fn"
